de10f37770d4109c1353418011d440e0f8ff88f4
hshar7 <r00tshaman@protonmail.com>
2019-04-28 15:55:52 -0500

    Fixing up notifications

--- src/main/kotlin/com/hshar/eggegg/controller/NotificationController.kt
+++ src/main/kotlin/com/hshar/eggegg/controller/NotificationController.kt
@@ -5,6 +5,7 @@ import com.hshar.eggegg.exception.ResourceNotFoundException
 import com.hshar.eggegg.repository.NotificationRepository
 import com.hshar.eggegg.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,7 +25,8 @@ class NotificationController {
     fun getNotificationsForUser(@PathVariable("userId") userId: String): String {
         val user = userRepository.findById(userId)
                 .orElseThrow{ ResourceNotFoundException("user", "userId", userId) }
-        val notifications = notificationRepository.findByUser(user)
+        val sort = Sort(Sort.Direction.DESC, "createdAt")
+        val notifications = notificationRepository.findByUser(user, sort)
 
         return Gson().toJson(notifications)
     }
--- src/main/kotlin/com/hshar/eggegg/repository/NotificationRepository.kt
+++ src/main/kotlin/com/hshar/eggegg/repository/NotificationRepository.kt
@@ -2,8 +2,9 @@ package com.hshar.eggegg.repository
 
 import com.hshar.eggegg.model.Notification
 import com.hshar.eggegg.model.User
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface NotificationRepository : MongoRepository<Notification, String> {
-    fun findByUser(user: User): List<Notification>
+    fun findByUser(user: User, sort: Sort): List<Notification>
 }
